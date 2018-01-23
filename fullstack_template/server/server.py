f4ee1ffd8ec572b7d9d373db77de64af2c790b2e
Erin Mahoney <erinmahoneyphoto@gmail.com>
2018-01-23 08:27:17 -0800

    server: set up routes and models
    Nominee and Category models are defined.
    Database defined and set up.  (Needs to be created still)
    "/" renders index.html template
    "/categories" queries and jsonify's categories
    "/submission" sets up to store submission in db, redirects to root.
    
    Form is updated with name to capture values.

--- fullstack_template/server/server.py
+++ fullstack_template/server/server.py
@@ -1,10 +1,52 @@
-from flask import Flask, render_template
+import os
 
-app = Flask(__name__, static_folder="../static/dist", template_folder="../static")
+from flask import Flask, redirect, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
-@app.route("/")
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "oscarNoms.db"))
+
+app = Flask(
+    __name__,
+    static_folder="../static/dist",
+    template_folder="../static")
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+
+db = SQLAlchemy(app)
+
+class Nominee(db.Model):
+    id = db.Column(db.Integer, nullable=False, primary_key=True)
+    category_id = db.Column(db.Integer, nullable=False)
+    name = db.Column(db.String, nullable=False)
+
+class Category(db.Model):
+    id = db.Column(db.Integer, nullable=False, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    points = db.Column(db.Integer, nullable=False)
+
+    def nominees(self):
+        return Nominee.query.filter_by(category_id=self.id).all()
+
+    # Do we need this?
+    # def __repr__(self):
+    #     return "<Answer: {}>".format(self.answer)
+
+@app.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
 
+@app.route("/categories", methods=["GET"])
+def categories():
+    categories = Category.query.all()
+    return jsonify(categories)
+
+# @app.route("/categories/:id/nominees", methods=["GET"])
+
+@app.route("/submission", methods=["POST"])
+def submission():
+    # print(request.form["erin"])
+    # store submissions in database
+    return redirect("/")
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
--- fullstack_template/static/js/components/Form/index.js
+++ fullstack_template/static/js/components/Form/index.js
@@ -3,20 +3,18 @@ import React from 'react';
 export default class Form extends React.PureComponent {
     render() {
         return (
-            <div className="col-6">
-                <form method="POST" action="/">
-                    <div className="form-group">
-                    <label>Some label here</label>
-                    <select className="form-control">
-                        <option value="example_1">example_1</option>
-                        <option value="example_2">example_2</option>
-                        <option value="example_3">example_3</option>
-                        <option value="example_4">example_4</option>
-                    </select>
-                    </div>
-                    <input type="submit" value="Add" className="btn btn-primary" />
-                </form>
-            </div>
+            <form method="POST" action="/submission">
+                <div className="form-group">
+                <label>Some label here</label>
+                <select className="form-control" name="erin">
+                    <option value="example_1">example_1</option>
+                    <option value="example_2">example_2</option>
+                    <option value="example_3">example_3</option>
+                    <option value="example_4">example_4</option>
+                </select>
+                </div>
+                <input type="submit" value="Add" className="btn btn-primary" />
+            </form>
         );
     }
 }
